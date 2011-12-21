adf9ddf801fbf57eb3ab9ff4a5829efbbbb7003b
Giuseppe Burtini <joe@truephp.com>
2011-12-21 13:41:20 -0800

    Completely functional update API now. Needs caching though.

--- ppu.php
+++ ppu.php
@@ -1,57 +1,83 @@
 <?php
+/*
+ * Very simple update class. Implements updates for non-WordPress codex plugins, in
+ * the form of a remote hosted JSON file (just like the codex uses).
+ *
+ * Example usage:
 
-define("PPU_UPDATE_FREQUENCY", 1);
+   $plugin = new PPU_Plugin();
+   $plugin->file = "madeupplugin.php";
+   $plugin->slug = "madeupplugin";
+   $plugin->version = 1;
+   $ppu = new PPU_Updater($plugin, "http://www.yoursite.com/update.json");
+
+ *
+ * Note, that as this is written right now, it makes the call to yoursite.com on
+ * every single page load. You will want to change this to be caching before deploying it.
+ */
 class PPU_Updater {
    private $plugin;
+   private $update;
    private $checkURL;
-   private $kvs;
 
-   function __construct($plugin, $checkURL, $kvs) {
+   function __construct($plugin, $checkURL) {
       $this->plugin = $plugin;
       $this->checkURL = $checkURL;
-      $this->kvs = $kvs;
 
       // set up hooks
-      add_filter("plugins_api", array(&$this, "plugins_api"));
+      add_filter("plugins_api", array(&$this, "plugins_api"), 10, 3);
       add_filter("site_transient_update_plugins", array(&$this, "site_transient_update_plugins"));
    }
 
    /*
     * returns the relevant update data to the WordPress.org update check
     */
-   public function plugins_api($deprecated, $action, $args) {
+   public function plugins_api($deprecated, $action=null, $args=null) {
       if($action != "plugin_information")
-         return false;
+        return false;
 
       if($args->slug != $this->plugin->slug)
          return false;
 
-      $update = $this->checkUpdate();
-      if(!$update);
-         return false;
+      add_action("ppu_adding_plugin_info", $this->plugin);
 
-      return (object) $update;
+      $obj = $this->checkUpdate();
+      return (object) $obj;
    }
 
    /*
     * adds the update nag to the plugins screen
     */
    public function site_transient_update_plugins($value) {
-      $update = $this->kvs->get("update");
+      add_action("ppu_adding_nag", $this->plugin);
+
+      $update = $this->checkUpdate();
+
+      if($update === null)
+         return $value;
+
       if(version_compare($this->plugin->version, $update->version, "<"))
          $value->response[$this->plugin->file] = (object) $update;
 
       return $value;
    }
 
-   public function checkUpdate() {
-      $url = add_query_arg("version", $this->plugin->version, $url);
+   public function checkUpdate($force=false) {
+      if(isset($this->update) && !$force)
+         return $this->update;
+
+      add_action("ppu_checking_update", $this->plugin);
+
+      $url = add_query_arg(
+         // this filter allows you to add licensing functionality (send the license along with the json request).
+         apply_filters("ppu_update_arguments", array("version" => $this->plugin->version))
+      , $this->checkURL);
+
 		$result = wp_remote_get($url);
       if(!is_wp_error($result))
       {
-         $update = new PPU_Plugin($result['body']);
-         $this->kvs->set("update", $update);
-         return $update;
+         $this->update = new PPU_Plugin($result['body']);
+         return $this->update;
       }
 
       return false;
@@ -59,9 +85,13 @@ class PPU_Updater {
 
 }
 
+// just a storage container for all the relevant variables.
 class PPU_Plugin {
    public $name;
    public $slug;
+
+   public $new_version;
+   public $package;
 
    public $version;
    public $requires;
@@ -82,12 +112,16 @@ class PPU_Plugin {
       if($json !== null)
          $this->loadJSON($json);
    }
-
+   private function castSectionsToArray()
+   {
+      $this->sections = get_object_vars($this->sections);
+   }
    public function loadJSON($json) {
       $response = get_object_vars(json_decode($json));
       foreach(($response) as $name=>$value)
       {
          $this->$name = $value;
       }
+      $this->castSectionsToArray();
    }
 }
