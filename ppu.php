bf642986ab3517658cf6017a49bb2cb4f83293eb
Giuseppe Burtini <joe@truephp.com>
2011-12-21 14:02:39 -0800

    Added license stuff.

--- ppu.php
+++ ppu.php
@@ -1,6 +1,21 @@
 <?php
 /*
- * Copyright © 2011 Giuseppe Burtini <joe@truephp.com>. All rights reserved.
+ * Copyright © 2011 Giuseppe Burtini <joe@truephp.com>. Most rights reserved.
+ *
+ *  The Private Plugin Updater is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    The Private Plugin Updater is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with the Private Plugin Updater.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Please contact me if you would like to discuss alternative licensing scenarios.
  *
  * Very simple update class. Implements updates for non-WordPress codex plugins, in
  * the form of a remote hosted JSON file (just like the codex uses).
